6601f95aa0ea2ea3546a0e47371455fb304eb50c
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-01 23:56:54 +0000

    Created DbContext

---
+++ Reviews/Data/ReviewDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reviews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reviews.Data
+{
+    public class ReviewDbContext : DbContext
+    {
+        public virtual DbSet<Account> Account { get; set; }
+        public virtual DbSet<Purchase> Purchase { get; set; }
+        public virtual DbSet <Review> Review { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=ReviewsDb;");
+        }
+    }
+}
