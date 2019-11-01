1a23fde9ed20322b583222868711f74c88cedf71
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-01 23:47:43 +0000

    Created Models

---
+++ Reviews/Models/Purchase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reviews.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int AccountId { get; set; }
+
+        public virtual Account Account { get; set; }
+    }
+}
