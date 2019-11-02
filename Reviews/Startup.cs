1752d357bcfa995edb68466a84fcc2974b37286b
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-02 12:11:56 +0000

    Added DbContext to startup w/ connexion string

--- Reviews/Startup.cs
+++ Reviews/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Reviews.Data;
 
 namespace Reviews
 {
@@ -33,6 +35,12 @@ namespace Reviews
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<ReviewDbContext>(options =>
+            {
+                var cs = Configuration.GetConnectionString("ReviewsConnection");
+                options.UseSqlServer(cs);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
