771669af073211ddca1e36f82656e55ef94ebec4
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-02 11:25:24 +0000

    Tweaked DbContext

--- Reviews/Data/ReviewDbContext.cs
+++ Reviews/Data/ReviewDbContext.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Reviews.Data
 {
@@ -11,11 +12,28 @@ namespace Reviews.Data
     {
         public virtual DbSet<Account> Account { get; set; }
         public virtual DbSet<Purchase> Purchase { get; set; }
-        public virtual DbSet <Review> Review { get; set; }
+        public virtual DbSet<Review> Review { get; set; }
+
+        private IHostingEnvironment HostEnv { get; set; }
+
+        public ReviewDbContext(DbContextOptions<ReviewDbContext> options, IHostingEnvironment env) : base(options)
+        {
+            HostEnv = env;
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=ReviewsDb;");
         }
-    }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            if (HostEnv != null && HostEnv.IsDevelopment())
+            {
+                //Seed Data
+            }
+        }
+}
 }
