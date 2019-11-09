a83cf1ec8043e9b37260952f3b279f682f672cb8
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-09 17:43:26 +0000

    Added Review Repository Interface

---
+++ Reviews/Data/IReviewRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Reviews.Models;
+
+namespace Reviews.Data
+{
+    public interface IReviewRepository
+    {
+        Review GetReview(int id);
+        void InsertReview(Review review);
+        void DeleteReview(int id);
+        void UpdateReview(Review review);
+        void HideReview(int id);
+        IEnumerable<Review> GetReviewsByProduct(int prodId);
+    }
+}
