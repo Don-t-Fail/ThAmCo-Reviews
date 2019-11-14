0fc9e0d182c84919922b23d63aabbbb43f8ac864
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-14 16:25:30 +0000

    Added Test for Getting Average review for an invalid product

--- Reviews.Tests/Controllers/ReviewsControllerTests.cs
+++ Reviews.Tests/Controllers/ReviewsControllerTests.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Reviews.Data;
 using Reviews.Models;
 
@@ -113,6 +115,44 @@ namespace Reviews.Controllers.Tests
 
             //Assert
             Assert.AreEqual(2, result.Value);
+        }
+
+        [TestMethod]
+        public void ProductAverage_NotExists()
+        {
+            //Arrange
+            var reviews = new List<Review>
+            {
+                new Review { Id = 1, Content = "Review No. 1", IsVisible = true, PurchaseId = 1, Rating = 1, Purchase = new Purchase
+                {
+                    AccountId = 1, Id = 1, ProductId = 1
+                }},
+                new Review { Id = 2, Content = "Review No. 2", IsVisible = false, PurchaseId = 4, Rating = 5, Purchase = new Purchase
+                {
+                    AccountId = 2, Id = 4, ProductId = 1
+                }},
+                new Review { Id = 4, Content = "Review No. 4", IsVisible = true, PurchaseId = 2, Rating = 3, Purchase = new Purchase
+                {
+                    AccountId = 1, Id = 2, ProductId = 1
+                }},
+                new Review { Id = 5, Content = "Review No. 5", IsVisible = false, PurchaseId = 3, Rating = 2, Purchase = new Purchase
+                {
+                    AccountId = 2, Id = 3, ProductId = 1
+                }},
+                new Review { Id = 3, Content = "Review No. 3", IsVisible = true, PurchaseId = 12, Rating = 3, Purchase = new Purchase
+                {
+                    AccountId = 3, ProductId = 4, Id =  12
+                }}
+            };
+            var repo = new FakeReviewRepository(reviews);
+            var controller = new ReviewsController(repo);
+            var prodId = 73;
+
+            //Act
+            var result = controller.ProductAverage(prodId);
+
+            //Assert
+            Assert.IsInstanceOfType(result.Result.Result, typeof(NotFoundResult));
         }
 
         [TestMethod]
