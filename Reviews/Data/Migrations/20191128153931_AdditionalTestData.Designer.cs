540cc3e9fc51d7bd2f9a2449013cbefffa05f69c
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-11-28 15:40:05 +0000

    Added Additional test data & migrated

--- Reviews/Data/Migrations/20191128153931_AdditionalTestData.Designer.cs
+++ Reviews/Data/Migrations/20191128153931_AdditionalTestData.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Reviews.Data;
 
 namespace Reviews.Migrations
 {
     [DbContext(typeof(ReviewDbContext))]
-    partial class ReviewDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191128153931_AdditionalTestData")]
+    partial class AdditionalTestData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Reviews/Data/ReviewDbContext.cs
+++ Reviews/Data/ReviewDbContext.cs
@@ -31,20 +31,27 @@ namespace Reviews.Data
                 builder.Entity<Account>().HasData(
                     new Account { Id = 1, IsStaff = false},
                     new Account { Id = 2, IsStaff = true},
-                    new Account { Id = 3, IsStaff = false}
+                    new Account { Id = 3, IsStaff = false},
+                    new Account { Id = 4, IsStaff = false },
+                    new Account { Id = 5, IsStaff = false }
                 );
 
                 builder.Entity<Purchase>().HasData(
                     new Purchase { Id = 1, AccountId = 1, ProductId = 1},
                     new Purchase { Id = 2, AccountId = 1, ProductId = 2},
-                    new Purchase { Id = 3, AccountId = 3, ProductId = 1}
+                    new Purchase { Id = 3, AccountId = 3, ProductId = 1},
+                    new Purchase { Id = 4, AccountId = 4, ProductId = 1 },
+                    new Purchase { Id = 5, AccountId = 3, ProductId = 3 },
+                    new Purchase { Id = 6, AccountId = 5, ProductId = 1 }
                 );
 
                 builder.Entity<Review>().HasData(
                     new Review { Id = 1, IsVisible = true, Rating = 5, Content = "This is a test review for product 1", PurchaseId = 1 },
                     new Review { Id = 2, IsVisible = true, Rating = 2, Content = "This is a review for product 2", PurchaseId = 2 },
-                    new Review { Id = 3, IsVisible = true, Rating = 5, Content = "This is a test review for product 1", PurchaseId = 3 }
-
+                    new Review { Id = 3, IsVisible = true, Rating = 3, Content = "This is a test review for product 1", PurchaseId = 3 },
+                    new Review { Id = 4, IsVisible = false, Rating = 1, Content = "This is a test review for product 1", PurchaseId = 4 },
+                    new Review { Id = 5, IsVisible = true, Rating = 3, Content = "This is a test review for product 3", PurchaseId = 5 },
+                    new Review { Id = 6, IsVisible = true, Rating = 3, Content = "This is a test review for product 1", PurchaseId = 6 }
                 );
             }
         }
