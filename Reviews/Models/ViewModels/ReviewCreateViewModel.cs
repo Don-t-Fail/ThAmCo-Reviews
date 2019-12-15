86f30bf20770e4f422698c2afa127f02e6e131d9
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-12-15 19:04:10 +0000

    Create ReviewCreateViewModel.cs

---
+++ Reviews/Models/ViewModels/ReviewCreateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reviews.Models.ViewModels
+{
+    public class ReviewCreateViewModel
+    {
+        [Display(Name = "Review ID")]
+        public int Id { get; set; }
+        [Display(Name = "Purchase ID")]
+        public int PurchaseId { get; set; }
+        [Display(Name = "Product Rating"), Range(0,5)]
+        public int Rating { get; set; }
+        [Display(Name = "Review Content")]
+        public string Content { get; set; }
+
+        public virtual Purchase Purchase { get; set; }
+    }
+}
