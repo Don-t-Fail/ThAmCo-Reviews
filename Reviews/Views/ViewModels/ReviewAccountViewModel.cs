bb3b941b09bfce512ed06ba9c723d178f52d6ea8
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-12-16 14:20:07 +0000

    Refactored ViewModels, added annotations

--- Reviews/Views/ViewModels/ReviewAccountViewModel.cs
+++ Reviews/Views/ViewModels/ReviewAccountViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,19 @@ namespace Reviews.Models.ViewModels
 {
     public class ReviewAccountViewModel
     {
+        [Display(Name = "Review ID")]
         public int Id { get; set; }
+        [Display(Name = "Purchase ID")]
         public int PurchaseId { get; set; }
+        [Display(Name = "Review Is Visible?")]
         public bool IsVisible { get; set; }
+        [Display(Name = "Account ID")]
         public int AccountId { get; set; }
+        [Display(Name = "Product ID")]
         public int ProductId { get; set; }
+        [Display(Name = "Rating")]
         public int Rating { get; set; }
+        [Display(Name = "Review Content")]
         public string Content { get; set; }
 
         public virtual Purchase Purchase { get; set; }
