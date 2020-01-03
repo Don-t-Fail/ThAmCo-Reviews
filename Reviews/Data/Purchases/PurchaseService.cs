1653bda40e723535cc681c8c1d8833a6a9ec0305
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-03 19:48:19 +0000

    Implemented Purchase Service

--- Reviews/Data/Purchases/PurchaseService.cs
+++ Reviews/Data/Purchases/PurchaseService.cs
@@ -1,16 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Reviews.Services;
 
 namespace Reviews.Data.Purchases
 {
     public class PurchaseService : IPurchaseService
     {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IConfiguration _config;
+        private readonly ILogger<PurchaseService> _logger;
+
+        public PurchaseService(IHttpClientFactory clientFactory, IConfiguration config, ILogger<PurchaseService> logger)
+        {
+            _clientFactory = clientFactory;
+            _config = config;
+            _logger = logger;
+        }
+
         public async Task<List<PurchaseDto>> GetAll()
         {
-            throw new NotImplementedException();
+            var client = _clientFactory.CreateClient("RetryAndBreak");
+            client.BaseAddress = new Uri(_config["PurchasesUrl"]);
+
+            _logger.LogInformation("Contacting Purchasing Service");
+
+            var resp = await client.GetAsync("purchases/GetAll");
+
+            if (resp.IsSuccessStatusCode)
+            {
+                var purchases = await resp.Content.ReadAsAsync<List<PurchaseDto>>();
+                return purchases;
+            }
+
+            return null;
         }
 
         public async Task<PurchaseDto> GetPurchase(int id)
