6066bf124ec187c546f973ff6243b7c14456b99d
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-03 19:44:44 +0000

    Created PurchaseDto

---
+++ Reviews/Services/PurchaseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reviews.Services
+{
+    public class PurchaseDto
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Qty { get; set; }
+        public int AddressId { get; set; }
+        public int AccountId { get; set; }
+        public string OrderStatus { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
