4831e258c31d29815a477780a1e1de4c684d6e7c
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-04 17:57:28 +0000

    Created test stubs

---
+++ Reviews.Tests/Data/Purchases/PurchaseServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reviews.Data.Purchases;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reviews.Data.Purchases.Tests
+{
+    [TestClass()]
+    public class PurchaseServiceTests
+    {
+        [TestMethod]
+        public async Task GetAll_Success()
+        {
+            Assert.Fail("Test Incomplete");
+        }
+
+        [TestMethod]
+        public async Task Get_Success()
+        {
+            Assert.Fail("Test Incomplete");
+        }
+
+        [TestMethod]
+        public async Task Get_NotExist()
+        {
+            Assert.Fail("Test Incomplete");
+        }
+
+        [TestMethod]
+        public async Task Get_OutOfBounds()
+        {
+            Assert.Fail("Test Incomplete");
+        }
+    }
+}
