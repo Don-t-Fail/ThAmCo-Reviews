38b161bdf72662fa03e00da47896d5c29e7c6c9e
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-06 16:22:02 +0000

    Created Accounts Controller & Views

---
+++ Reviews/Controllers/AccountsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Reviews.Models;
+
+namespace Reviews.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public AccountsController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+    }
+}
