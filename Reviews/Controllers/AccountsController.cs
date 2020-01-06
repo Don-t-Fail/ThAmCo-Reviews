71e7075767315221646f248a687787e7cb811770
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-06 17:32:00 +0000

    Implemented login in controller

--- Reviews/Controllers/AccountsController.cs
+++ Reviews/Controllers/AccountsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using IdentityModel.Client;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,79 @@ namespace Reviews.Controllers
         public async Task<IActionResult> Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromForm] LoginModel model)
+        {
+            var client = GetHttpClient("RetryAndBreak");
+
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:43389");
+            var tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "my_web_app",
+                ClientSecret = "secret",
+
+                UserName = model.Email,
+                Password = model.Password
+            });
+
+            if (tokenResponse.IsError)
+                return View();
+
+            var userInfoResponse = await client.GetUserInfoAsync(new UserInfoRequest
+            {
+                Address = disco.UserInfoEndpoint,
+                Token = tokenResponse.AccessToken
+            });
+
+            if (userInfoResponse.IsError)
+                return View();
+
+            var claimsIdentity = new ClaimsIdentity(userInfoResponse.Claims, "Cookies");
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+            var tokensToStore = new AuthenticationToken[]
+            {
+                new AuthenticationToken { Name = "access_token", Value = tokenResponse.AccessToken }
+            };
+            var authProperties = new AuthenticationProperties();
+            authProperties.StoreTokens(tokensToStore);
+
+            await HttpContext.SignInAsync("Cookies", claimsPrincipal, authProperties);
+
+            return LocalRedirect("/");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            return Ok("Signed Out");
+        }
+
+        [Authorize]
+        public IActionResult Authed()
+        {
+            return Ok("Authed");
+        }
+
+        [Authorize(Policy = "StaffOnly")]
+        public IActionResult StaffAuthed()
+        {
+            return Ok("Authed");
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        private HttpClient GetHttpClient(string s)
+        {
+            return _clientFactory.CreateClient(s);
         }
     }
 }
