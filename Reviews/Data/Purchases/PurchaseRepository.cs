364684b05080e88eb57718ca330a078be0b61799
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-07 01:00:41 +0000

    Created PurchaseRepository

---
+++ Reviews/Data/Purchases/PurchaseRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Reviews.Models;
+
+namespace Reviews.Data.Purchases
+{
+    public class PurchaseRepository : IPurchaseRepository
+    {
+        private readonly ReviewDbContext _context;
+
+        public PurchaseRepository(ReviewDbContext context)
+        {
+            _context = context;
+        }
+        public async Task InsertPurchase(Purchase purchase)
+        {
+            await _context.Purchase.AddAsync(purchase);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
